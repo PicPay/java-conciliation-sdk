52e1a2e68031aa67a9117f5f0cb23dcee843918a
Rodrigo Pinto <picpay@PPM-SPO-06326.local>
2020-10-16 13:34:59 -0300

    Estrutura básica da aplicação

---
+++ src/test/java/com/picpay/javaconciliationsdk/JavaConciliationSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.javaconciliationsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConciliationSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
